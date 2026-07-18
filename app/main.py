67bb7c25d525943ea4f2b02c1139af8be1e489c1
Asshley <asshleydeanwaynegozar@gmail.com>
2026-07-18 22:29:52 +0800

    implement simple api hello message

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import fastapi
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return {"message": "Welcome to the Feedback Insights API!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8080,  
+        reload=True
+    )
+
